19aec28d221adc9ae55439fae8e7312d9cc48fa3
Humaluka <humaluka@gmail.com>
2023-03-11 13:39:57 +0300

    Add UnitTests

---
+++ UnitTestMindbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMindbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMindbox")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fbc7eed-5861-45d6-9c5a-00aac694bf40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testTaskMindbox/Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testTaskMindbox
+{
+    internal class Example
+    {
+        static void Main(string[] args)
+        { 
+            // Создание круга
+            double circleRadius = 5;
+            var circle = new Geometry.Circle(circleRadius);
+            double circleArea = circle.Area(); // Вычисление площади круга
+            Console.WriteLine($"Площадь круга с радиусом {circleRadius} = {circle.Area()}");
+
+            // Создание треугольника
+            double triangleSide1 = 3;
+            double triangleSide2 = 4;
+            double triangleSide3 = 5;
+            var triangle = new Geometry.Triangle(3, 4, 5);
+            double triangleArea = triangle.Area(); // Вычисление площади треугольника
+            Console.WriteLine($"Площадь треугольника со сторонами {triangleSide1}, {triangleSide2} и {triangleSide3} = {triangle.Area()}");
+            Console.WriteLine($"Треугольник прямоугольный? {triangle.IsRightTriangle()}");
+
+            // Вычисление площади фигуры без знания типа фигуры в compile-time
+            dynamic shape = circle;
+            double area = Geometry.CalculateArea(shape); // Вычисление площади круга
+            Console.WriteLine($"Площадь фигуры без знания типа в compile-time = {Geometry.CalculateArea(shape)}");
+
+            shape = triangle;
+            area = Geometry.CalculateArea(shape); // Вычисление площади треугольника
+            Console.WriteLine($"Площадь фигуры без знания типа в compile-time = {Geometry.CalculateArea(shape)}");
+        }
+    }
+}
